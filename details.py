99e8a5116230aa52f7b58899a1d8845aa373d935
Daniel Mukenya <88248852+watchout254@users.noreply.github.com>
2023-10-06 10:16:13 +0300

    Finally!!!!

--- details.py
+++ details.py
@@ -1,25 +1,30 @@
 import qrcode
 from pyzbar.pyzbar import decode
 from PIL import Image
-import sys
 
 print(f"Welcome to Mukenya's School\n"
       "After filling in your detals, open your qr code scanner and scan the qr code to save your details")
+
+data = {}
 
 name = input("Name: ")
 reg_no = int(input("Registration Number: "))
 course = input("Course: ")
 dept = input("Department: ")
-stu_details = print(f"{name},{reg_no},{course},{dept} is logged in")
+
+data[name] = reg_no, course, dept
+
+img = qrcode.make(data)
+qr = qrcode.QRCode(version = 1, box_size = 10, border = 5)
+qr.add_data(data)
+qr.make(fit = True)
+img = qr.make_image(fill_color = "blue", back_color = "white" )
+img.save('C:/Users/Dan/Desktop/qrcode/image6.png')
+
+print("You can now scan the code. Thank you my g...")
 
 
-image_2 = qrcode.make(stu_details)
-cheki = qrcode.QRCode(version = 1, box_size = 10, border = 5)
-cheki.add_data(stu_details)
-cheki.make(fit = True)
-image_2 = cheki.make_image(fill_color = "yellow", back_color = "blue" )
-image_2.save('C:/Users/Dan/Desktop/qrcode/image2.png')
-
-#To decode go to decode.py file
 
 
+
+ 
